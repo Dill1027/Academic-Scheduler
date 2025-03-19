cea7324e08e9f90dab35a0177d6365b334deb043
Dilshani Senavirathna <141391107+Dilshani16@users.noreply.github.com>
2025-03-19 12:36:27 +0530

    coursedash

--- frontend/src/Components/CourseManagement/coursedash.js
+++ frontend/src/Components/CourseManagement/coursedash.js
@@ -2,6 +2,9 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import SideBar from "../Home/SideBar";
 import Header from "../Navbar/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import './coursed.css';
+import { Link } from 'react-router-dom';
 
 function Coursed() {
 
@@ -11,24 +14,83 @@ function Coursed() {
       <div className="main_function ">
         <div>
           <Header />
-          {/* <SideBar /> */}
+          
+       <div style={{ marginTop: "20px" , padding:'125px' }}>
+        
+            <div className="p1">
+                <Link to="/module-allocation" style={{ textDecoration: 'none' , color:'white'}}>
+                   <h3>Module Allocation for Students</h3>
+                </Link>
+            </div>
 
-           <div class="mt-5">
-             {/* <h1>lef child</h1> */}
+      {/* Course Requirements Section */}
+            <div className="p1 mt-5">
+                <Link to="/upload-course-requirements" style={{ textDecoration: 'none' }}>
+                   <h3>Uploading Course Requirements</h3>
+                </Link>
+            </div>
 
 
+             <div className=" p1 container mt-5">
+                <h3>Available Courses</h3>
+
+                 
+                <div className=" row mt-4">
+       
+                     <div className="col-md-3 mb-4">
+                        <div className="card">
+                            <div className="card-body text-center">
+                               <h4 className="card-title">1st Year</h4>
+                               <p className="card-text">View courses for 1st-year students</p>
+                               <Link to="/1st-year" className="btn btn-primary">Go to 1st Year</Link>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* 2nd Year */}
+                    <div className="col-md-3 mb-4">
+                    <div className="card">
+                        <div className="card-body text-center">
+                        <h4 className="card-title">2nd Year</h4>
+                        <p className="card-text">View courses for 2nd-year students</p>
+                        <Link to="/2nd-year" className="btn btn-primary">
+                            Go to 2nd Year
+                        </Link>
+                        </div>
+                    </div>
+                    </div>
+
+                    {/* 3rd Year */}
+                    <div className="col-md-3 mb-4">
+                    <div className="card">
+                        <div className="card-body text-center">
+                        <h4 className="card-title">3rd Year</h4>
+                        <p className="card-text">View courses for 3rd-year students</p>
+                        <Link to="/3rd-year" className="btn btn-primary">
+                            Go to 3rd Year
+                        </Link>
+                        </div>
+                    </div>
+                    </div>
+
+                    {/* 4th Year */}
+                    <div className="col-md-3 mb-4">
+                    <div className="card">
+                        <div className="card-body text-center">
+                        <h4 className="card-title">4th Year</h4>
+                        <p className="card-text">View courses for 4th-year students</p>
+                        <Link to="/4th-year" className="btn btn-primary">
+                            Go to 4th Year
+                        </Link>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
 
            </div>
         </div>
 
-
-
-
-       
-        {/* <div className="lef_child">
-        <h1 className="main_topic_function">Add Employee </h1>
-          
-        </div> */}
       </div>
     </div>
   );
