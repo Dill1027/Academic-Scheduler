93bbd022ecef5aec6aa5ef19a6e2ce5f394ecb3d
Dilshani Senavirathna <141391107+Dilshani16@users.noreply.github.com>
2025-05-04 14:30:32 +0530

    student list

--- frontend/src/Components/StudentManagement/StudentList.js
+++ frontend/src/Components/StudentManagement/StudentList.js
@@ -15,7 +15,7 @@ const StudentList = () => {
   useEffect(() => {
     const fetchStudents = async () => {
       try {
-        const response = await axios.get('http://localhost:6001/api/student');
+        const response = await axios.get('http://localhost:5000/api/student');
         setStudents(response.data);
         setIsLoading(false);
       } catch (error) {
